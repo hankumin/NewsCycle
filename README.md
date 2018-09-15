b9632804b48cec05cce412aaf00b19f70ad4d866
hankumin <chan4@stevens.edu>
2018-09-14 22:33:59 -0400

    testing branches

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # NewsCycle
-Summer 2018 Internship
+Summer 2018 Internship (June-August)
 
-Worked with company NewsCycle in creating python scripts that help accomplish specific tasks such as looking for problematic phrases or 
+Worked with company NewsCycle in creating python scripts that help accomplish specific tasks such as looking for problematic phrases or
 checking MySql tables
 
 Also attempts to use CSV module
--- adam_tickery.py
+++ adam_tickery.py
@@ -1,40 +1,41 @@
 #/usr/bin/python
+#/Testing some stuff
 from fabric.api import *
 import sys
 #edi_path = "/nwsys/tickergen-EDI/"
 met_sun= "/nwsys/metabot_sungard/"
 edi_path = met_sun
 list_of_files = [
-                  'xwalk_in.txt', 
-                  'dnbInput.txt', 
-                  'sungardDomestic.txt', 
-                  'sungardIntl.txt', 
-                  'xwalkOverride.txt', 
-                  'mapper/ticker_secid_name.map', 
-                  'mapper/mapper_override', 
-                  'mapper/NaicsToIc.txt', 
-                  'mapper/sungardPreferredTickers.txt', 
-                  'noah/sungard_name_overrides', 
-                  'noah/sungard_symbols_override', 
-                  'noah/sungard_ticker_overrides', 
-                  'noah/location_variants', 
-                  'noah/lastAcorn', 
-                  'noah/dict.amc', 
-                  'noah/company_variants', 
-                  'noah/peoplenames', 
-                  'noah/locations', 
-                  'noah/symbols', 
-                  'compustat/domestic.txt', 
-                  'compustat/foreign.txt', 
-                  'working/NewConcordance', 
-                  'working/concordance' 
+                  'xwalk_in.txt',
+                  'dnbInput.txt',
+                  'sungardDomestic.txt',
+                  'sungardIntl.txt',
+                  'xwalkOverride.txt',
+                  'mapper/ticker_secid_name.map',
+                  'mapper/mapper_override',
+                  'mapper/NaicsToIc.txt',
+                  'mapper/sungardPreferredTickers.txt',
+                  'noah/sungard_name_overrides',
+                  'noah/sungard_symbols_override',
+                  'noah/sungard_ticker_overrides',
+                  'noah/location_variants',
+                  'noah/lastAcorn',
+                  'noah/dict.amc',
+                  'noah/company_variants',
+                  'noah/peoplenames',
+                  'noah/locations',
+                  'noah/symbols',
+                  'compustat/domestic.txt',
+                  'compustat/foreign.txt',
+                  'working/NewConcordance',
+                  'working/concordance'
                 ]
 # removed mapper/exchanges.csv
 def copy_from_cameo2():
 
     with settings(host_string="cameo2.mmu.acquiremedia.com", user = "nadmin", password="nadmin"):
          for file in list_of_files :
-             try: 
+             try:
                    path = edi_path+file
                    path = path[:path.rfind('/')]
                    local('mkdir -p ' + path)
@@ -44,4 +45,3 @@ def copy_from_cameo2():
 
 if __name__ == "__main__":
    copy_from_cameo2()
-
